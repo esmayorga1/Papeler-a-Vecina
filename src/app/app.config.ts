93f7d039b203a7d5f8b1d7bc7e9881a55108db25
Edison Mayorga <mayorga.sebas100@hotmail.com>
2024-10-15 17:13:06 -0500

    Correciones en el login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,36 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  firebaseConfig: {
+    apiKey: "AIzaSyALrj1bXtHleGKa2vioiE1TT2KZGFoLK6g",
+    authDomain: "papeleriavecina-85919.firebaseapp.com",
+    projectId: "papeleriavecina-85919",
+    storageBucket: "papeleriavecina-85919.appspot.com",
+    messagingSenderId: "1037392817914",
+    appId: "1:1037392817914:web:b57b1d23513b002fbf6343",
+    measurementId: "G-688S0F8KSC"   
+    
+  
+  }
+};
+  
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), 
+    provideClientHydration(),
+    importProvidersFrom(
+      HttpClientModule,
+      AngularFireModule.initializeApp(environment.firebaseConfig),
+      AngularFireModule,
+      AngularFireAuthModule
+    )
+  ]
 };
---
+++ src/app/components/autenticacion/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    const email = (document.getElementById('emailUsuario') as HTMLInputElement).value;
+    const password = (document.getElementById('passwordUsuario') as HTMLInputElement).value;
+  
+    this.authService.loginWithEmail(email, password)
+      .then(() => {
+        console.log("Inicio de sesión exitoso");
+        //this.router.navigate(['/admin']); 
+      })
+      .catch(error => {
+        // Manejar errores específicos
+        if (error.code === 'auth/user-not-found') {
+          alert("Parece que no tienes una cuenta con este correo. ¿Quieres registrarte?");
+        } else if (error.code === 'auth/wrong-password') {
+          alert("La contraseña que ingresaste es incorrecta. Por favor, inténtalo de nuevo.");
+        } else if (error.code === 'auth/invalid-email') {
+          alert("El formato del correo electrónico no es válido. Asegúrate de escribirlo correctamente.");
+          
+        }          
+      
+        
+        
+        else {
+          alert("Se ha producido un error inesperado. Por favor, inténtalo más tarde o contacta a nuestro equipo de soporte.");
+        }
+        console.error("Error al iniciar sesión:", error);
+      });    
+      
+  
+      
+  }}
+
